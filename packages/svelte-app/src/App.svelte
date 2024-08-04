b386cc83e6829c6d88fdb1a7e866502d0bf09963
Samir <bettahar.samir@outlook.com>
2024-08-04 07:24:01 +0100

    feat: creating todo app and an astrojs app

---
+++ packages/svelte-app/src/App.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import './app.css';
+	import { v4 as uuidv4 } from 'uuid';
+	import TaskList from "./components/TaskList.svelte";
+	import TaskInput from './components/TaskInput.svelte';
+
+  
+	let tasks: { id: string; name: string }[] = [];
+  
+	const addTask = (event: CustomEvent) => {
+	  tasks = [...tasks, { id: uuidv4(), name: event.detail.name }];
+	};
+  
+	const deleteTask = (id: string) => {
+	  tasks = tasks.filter((task) => task.id !== id);
+	};
+  </script>
+  
+  <div class="flex items-center justify-center h-screen border-red-600 border"> 
+	<div class="p-4 max-w-sm mx-auto bg-white rounded-xl shadow-md">
+
+		<h1 class="text-xl font-bold">To-Do List</h1>
+		<TaskInput on:addtask={addTask} />
+		<TaskList {tasks} {deleteTask} />
+	</div>
+</div>
+  
---
+++ packages/svelte-app/src/components/TaskInput.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import Icon from '@iconify/svelte';
+    import { createEventDispatcher } from 'svelte';
+    const dispatch = createEventDispatcher();
+  
+    let newTaskName: string = '';
+  
+    const submitForm = () => {
+      if (newTaskName.trim()) {
+        dispatch('addtask', { name: newTaskName });
+        newTaskName = '';
+      }
+    };
+  </script>
+  
+  <form on:submit|preventDefault={submitForm} class="flex items-center space-x-2">
+    <input
+      class="border p-2 rounded"
+      type="text"
+      bind:value={newTaskName}
+      placeholder="Add a new task"
+    />
+    <button type="submit" class="bg-blue-500 text-white p-2 rounded">
+      <Icon icon="mdi:plus-circle" />
+    </button>
+  </form>
+  
